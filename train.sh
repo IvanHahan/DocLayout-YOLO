183d2d74d748257aef006aa3d05cb013fc399e33
IvanHahan <ivan.hahanov@stereologic.com>
2024-11-15 19:41:56 +0200

    update training params

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --data interactable --model m-doclayout --epoch 500 --image-size 512 --batch-size 1 --project interactable --plot 1 --optimizer SGD --lr0 0.04 --pretrain interactable_detector.pt --device=0
+python train.py --data interactable --model m-doclayout --epoch 100 --image-size 1600  --batch-size 8 --project outputs --plot 1 --optimizer SGD --lr0 0.04 --pretrain interactable_detector.pt --device=0 --save-period 100 --val-period 10
